203d924cbe58cc213f1687d65c7b29b48a8d4f9c
Phil B <phil@berryman.org.uk>
2019-03-19 14:36:41 +0000

    styled

--- client/src/QualificationForm.js
+++ client/src/QualificationForm.js
@@ -2,6 +2,8 @@ import React from "react";
 import { Formik, Form, Field } from "formik";
 import * as Yup from "yup";
 import {
+  StyledForm,
+  StyledField,
   FormGrid,
   FormQuestions,
   FormContent,
@@ -9,7 +11,8 @@ import {
   Input,
   Label,
   Select,
-  ErrorMessage,
+  Button,
+  FormError,
   MarketingHeader,
 } from "./styles/styles";
 
@@ -18,10 +21,7 @@ const Schema = Yup.object().shape({
     .min(2, "Must be longer than 2 characters")
     .max(20, "Nice try, nobody has a first name that long")
     .required("Required"),
-  annualIncome: Yup.string()
-    .min(2, "Must be longer than 2 characters")
-    .max(20, "Nice try, nobody has a last name that long")
-    .required("Required"),
+  annualIncome: Yup.string().required("Required"),
 });
 
 const onSubmit = history => history.push("/select-cards");
@@ -55,54 +55,60 @@ export const QualificationForm = ({ setUserQualificationDetails, history }) => {
           }}
           render={({ errors, touched, isValid, handleChange, handleBlur }) => (
             <Form>
-              <Label htmlFor="employmentStatus">
-                Employment Status
-                <Select
-                  onChange={handleChange}
-                  name="employmentStatus"
-                  placeholder="Employed"
-                  type="text"
+              <StyledForm>
+                <Label htmlFor="employmentStatus">Employment</Label>
+                <Input>
+                  <Select
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    name="employmentStatus"
+                    placeholder="Employed"
+                    type="text"
+                    error={errors.employmentStatus}
+                  >
+                    <option value="">--Please choose an option--</option>
+                    <option value="full time employment">
+                      Full Time Employment
+                    </option>
+                    <option value="Part Time Employment">
+                      Part Time Employment
+                    </option>
+                    <option value="student">Student</option>
+                    <option value="self employed">Self Employed</option>
+                    <option value="pensioner">Pensioner</option>
+                    <option value="unemployed">Unemployed</option>
+                    <option value="other">Other</option>
+                  </Select>
+                  <FormError>{errors.employmentStatus}</FormError>
+                </Input>
+
+                <Label htmlFor="annualIncome">Annual Income</Label>
+                <Input>
+                  <Field
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    name="annualIncome"
+                    id="annualIncome"
+                    placeholder="10,000"
+                    type="number"
+                    autocomplete="off"
+                    component={StyledField}
+                    error={errors.annualIncome && touched.annualIncome}
+                  />
+                  <FormError>
+                    {errors.annualIncome && touched.annualIncome
+                      ? errors.annualIncome
+                      : null}
+                  </FormError>
+                </Input>
+                <Button
+                  type="submit"
+                  onSubmit={() => onSubmit()}
+                  disabled={!isValid}
                 >
-                  <option value="">--Please choose an option--</option>
-                  <option value="full time employment">
-                    Full Time Employment
-                  </option>
-                  <option value="Part Time Employment">
-                    Part Time Employment
-                  </option>
-                  <option value="student">Student</option>
-                  <option value="self employed">Self Employed</option>
-                  <option value="pensioner">Pensioner</option>
-                  <option value="unemployed">Unemployed</option>
-                  <option value="other">Other</option>
-                </Select>
-              </Label>
-              <Label htmlFor="annualIncome">
-                Annual Income
-                <br />
-                <Field
-                  onChange={handleChange}
-                  onBlur={handleBlur}
-                  name="annualIncome"
-                  id="annualIncome"
-                  placeholder="10,000"
-                  type="text"
-                  autocomplete="off"
-                  component={Input}
-                  error={errors.annualIncome && touched.annualIncome}
-                />
-                {errors.annualIncome && touched.annualIncome ? (
-                  <ErrorMessage>{errors.annualIncome}</ErrorMessage>
-                ) : null}
-              </Label>
-              <br />
-              <button
-                type="submit"
-                onSubmit={() => onSubmit()}
-                disabled={!isValid}
-              >
-                Submit
-              </button>
+                  Submit
+                </Button>
+              </StyledForm>
             </Form>
           )}
         />
